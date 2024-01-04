c7d6ed373c729afc01f13331f93c8922ce350303
August Zhang <azhan157@uottawa.ca>
2024-01-04 13:16:56 -0500

    Proper Main file

--- Desktop/Discord Bot/main.py
+++ Desktop/Discord Bot/main.py
@@ -4,8 +4,8 @@ import responses
 
 
 if __name__ == '__main__':
-    TOKEN = 'MTE1MjI1NTI2MzQyOTExMTg4OQ.G4tUZx.gmZERnvvuvTpYJMzPfRZ0ydboR7LuxKik2FDAw'  # Replace with your bot token
+    TOKEN = 'MTE1MjI1NTI2MzQyOTExMTg4OQ.G4tUZx.gmZERnvvuvTpYJMzPfRZ0ydboR7LuxKik2FDAw'
     
-    # Assuming bot and client are instances of commands.Bot and discord.Client respectively
+    
     bot.bot.run(TOKEN)
     client.client.run(TOKEN)
